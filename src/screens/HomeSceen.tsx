1d4609aa344d17b786be514eec57308ae613229b
tudev <tukieu954@gmail.com>
2024-07-04 00:51:09 +0700

    nagivation , tab bottom , apicall setup

--- src/screens/HomeSceen.tsx
+++ src/screens/HomeSceen.tsx
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { COLORS } from '../theme/theme'
 
-const HomeSceen = () => {
+
+
+
+const HomeSceen = ({navigation}:any) => {
   return (
-    <View>
-      <Text>HomeSceen</Text>
+    <View style = {styles.container}>
+      <TouchableOpacity onPress={() => {
+        navigation.push('MovieDetails')
+      }}>
+        <Text style={{color: COLORS.Black, fontSize: 20 }}>Home</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default HomeSceen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    // backgroundColor: COLORS.Black,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
